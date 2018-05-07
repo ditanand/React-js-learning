a2b45db2318b3aafd03d897a6d05bc6b7c0f639e
Mohd Sameer <mohdsameer1310@gmail.com>
2018-05-07 15:37:51 +0530

    js can be enclosed with {} tag

--- src/index.js
+++ src/index.js
@@ -2,28 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-class Hello extends React.Component{
-	render(){
-		return(
-			<h1>Hello</h1>
-	    )
-	}
-}
-
-class World extends React.Component{
-	render(){
-		return(
-		  <p>world</p>
-		)
-	}
-}
-
 class Helloworld extends React.Component{
+  getSum(){
+  	return (2 + 2)
+  }
   render(){
   	return(
 	  	<section className="foo">
-	  	  <Hello/>
-	  	  <World/> 
+         {this.getSum()}
 	  	</section>
   	)
   }

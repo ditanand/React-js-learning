8818e08ed49680eb08081d2efdf070c34498e209
Mohd Sameer <mohdsameer1310@gmail.com>
2018-05-08 14:51:14 +0530

    value can be passed to child component via props

--- src/index.js
+++ src/index.js
@@ -2,19 +2,35 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-class Helloworld extends React.Component{
+class List extends React.Component{
   // We can also use contructor function call to initilize value, and its first line should be to call super for Reac.componet
   constructor(){
     super();
-    this.name = "Anand"
+    // state is component specific
+    this.state = {
+      firstName: "Anand"
+    }
   }
   render(){
   	return(
-	  	<section className="foo">
-          {this.name}
-	  	</section>
+      <ul>
+        // value can be passed to child component via props
+        <Item firstname={this.state.firstName} lastname="kumar" />
+      </ul>
   	)
   }
 }
 
-ReactDom.render(<Helloworld/>, document.getElementById("root"));
+class Item extends React.Component{
+  render(){
+    return(
+      <li>
+       {this.props.firstname}
+       <br/>
+       {this.props.lastname}
+      </li>
+    )
+  }
+}
+
+ReactDom.render(<List/>, document.getElementById("root"));

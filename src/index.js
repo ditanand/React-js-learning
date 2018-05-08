5abb69372018120209cad59c8f5b0d4ac83d2427
Mohd Sameer <mohdsameer1310@gmail.com>
2018-05-08 14:19:15 +0530

    constructor call in react

--- src/index.js
+++ src/index.js
@@ -3,19 +3,15 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 class Helloworld extends React.Component{
-  getSum(){
-  	return (2 + 2)
-  }
-
-  getName(){
-  	return "Anand";
+  // We can also use contructor function call to initilize value, and its first line should be to call super for Reac.componet
+  constructor(){
+    super();
+    this.name = "Anand"
   }
   render(){
   	return(
 	  	<section className="foo">
-         {this.getName()}
-         <br/>
-         {this.getSum()}
+          {this.name}
 	  	</section>
   	)
   }
